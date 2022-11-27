dae7ac9e3a69ce53e80d32a0309437ca9bd9eb32
Даниил <geimer_cok@mail.ru>
2022-11-27 21:27:53 +0300

    Вторая задача

---
+++ Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Enter one numbers: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a>99)
+{
+    int N = (a%100) / 10;
+    Console.WriteLine(N);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
