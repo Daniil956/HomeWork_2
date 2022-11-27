5a3e2957b58856f396076ce300d7f3d18dcf274b
Даниил <geimer_cok@mail.ru>
2022-11-27 21:35:28 +0300

    третьлья задача

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Enter one numbers: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 6 || a == 7)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
